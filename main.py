6214be64ad53c9d11718b8d467eb8aef2e4d2be1
AlexandreNevesdeFreitas <alexandre.neves@estudante.ifgoiano.edu.br>
2024-06-17 16:00:08 -0300

    Criação de arquivo + correção tempo

--- main.py
+++ main.py
@@ -4,44 +4,53 @@ import matplotlib.pyplot as plt
 from metodos import *
 
 
-
-inicio = time.time()
 raio = 10
 centro = np.array([0, 0, 0])
 
 eixo_cone = np.array([0, 0, 1])
 
 theta = np.pi / 4
-sorteios = [10**3, 10**4, 10**6]
+sorteios = [10**0, 10**1, 10**2, 10**3, 10**4, 10**5]
+# , 10**6, 10**7, 10**8]
 
 resultados = []
 valores = []
 
+arquivo = "resultados.txt"
+
+inicio = time.time()
 for i, qtde_sorteios in enumerate(sorteios):
+    tempo_inicio = time.time()
     pts_no_angulo_solido = sortea_pontos(qtde_sorteios, raio, centro, eixo_cone, theta)
+    tempo_fim = time.time()
 
     valor = ((4 * np.pi) * pts_no_angulo_solido) / qtde_sorteios
+    tempo_gasto = tempo_fim - tempo_inicio
+    gerar_arquivo(sorteios[i], valor, tempo_gasto, arquivo)
+     
     novo_resultado = {
-        "Pontos no ângulo sólido: ": pts_no_angulo_solido, 
-        "Valor do ângulo sólido": valor
+        "Pontos no Ã¢ngulo sÃ³lido: ": pts_no_angulo_solido, 
+        "Valor do Ã¢ngulo sÃ³lido": valor
     }
     resultados.append(novo_resultado)
     valores.append(valor)
 
-fig, axs = plt.subplots(1, len(resultados), figsize=(18, 6))
+fim =  time.time()
+
+
+fig, axs = plt.subplots(len(resultados), 1, figsize=(6,10))
 
 for i, resultado in enumerate(resultados):
-    pts_no_angulo_solido = resultado["Pontos no ângulo sólido: "]
+    pts_no_angulo_solido = resultado["Pontos no Ã¢ngulo sÃ³lido: "]
     qtde_sorteios = sorteios[i]
     obter_resultados(pts_no_angulo_solido, qtde_sorteios, ax=axs[i])
+
 
 print(resultados)
 obter_tendencia_angulo_solido(valores, sorteios)
 
 plt.tight_layout()
 plt.show()
-
-fim =  time.time()
 
 tempo_execucao = fim - inicio
 print("Tempo: ", tempo_execucao)
--- metodos.py
+++ metodos.py
@@ -33,8 +33,6 @@ def sortea_pontos(qtde_sorteios, raio, centro, eixo, theta):
         cos_angulo = projecao_vetor / comprimento_vetor
         cos_theta = np.cos(theta)
 
-
-
         if cos_angulo >= cos_theta:
             pts_no_angulo_solido += 1
     
@@ -44,11 +42,11 @@ def obter_resultados(pts_no_angulo_solido, qtde_sorteios, ax=None):
     porcentagem_dentro = Decimal((pts_no_angulo_solido / qtde_sorteios) * 100)
     porcentagem_fora = Decimal(100 - porcentagem_dentro)
 
-    rotulos = ['Dentro do ângulo sólido', 'Fora do ângulo sólido']
+    rotulos = ['Dentro do Ã¢ngulo sÃ³lido', 'Fora do Ã¢ngulo sÃ³lido']
     porcentagens = [porcentagem_dentro, porcentagem_fora]
 
     if ax is None:
-        fig, ax = plt.subplots(figsize=(6, 6))
+        fig, ax = plt.subplots(figsize=(6, 10))
     bars = ax.bar(rotulos, porcentagens, color=['green', 'gray'])
     ax.set_ylabel('Porcentagem')
     ax.set_title(f'sorteios: {qtde_sorteios}')
@@ -64,29 +62,19 @@ def obter_tendencia_angulo_solido(valores_angulo_solido, sorteios, ax=None):
     pi = np.pi
     
     plt.figure(figsize=(10, 6))
-    plt.plot(sorteios, valores_angulo_solido, marker='o', linestyle='-', color='b', label='Valores do ângulo sólido')
+    plt.plot(sorteios, valores_angulo_solido, marker='o', linestyle='-', color='b', label='Valores do Ã¢ngulo sÃ³lido')
 
-    plt.plot(sorteios, [pi] * len(sorteios), linestyle='--', color='r', label='π (3.14159...)')
+    plt.plot(sorteios, [pi] * len(sorteios), linestyle='--', color='r', label='Ï (3.14159...)')
 
-    plt.title('Tendência do Ângulo Sólido em Relação a π')
-    plt.xlabel('Número de Sorteios')
-    plt.ylabel('Resultado (Ângulo Sólido)')
+    plt.title('TendÃªncia do Ãngulo SÃ³lido em RelaÃ§Ã£o a Ï')
+    plt.xlabel('NÃºmero de Sorteios')
+    plt.ylabel('Resultado (Ãngulo SÃ³lido)')
     plt.xticks(sorteios)
     plt.legend()
     plt.grid(True)
-    # pi = np.pi
-    # if ax is None:
-    #     fig, ax = plt.subplots(figsize=(10, 6))
-    # ax.plot(sorteios, valores_angulo_solido, marker='o', linestyle='-', color='b', label='Valores do ângulo sólido')
 
-    # # Adicionando linha de tendência para pi
-    # ax.plot(sorteios, [pi] * len(sorteios), linestyle='--', color='r', label='π (3.14159...)')
 
-    # ax.set_title('Tendência do Ângulo Sólido em Relação a π')
-    # ax.set_xlabel('Número de Sorteios')
-    # ax.set_ylabel('Resultado (Ângulo Sólido)')
-    # ax.set_xticks(sorteios)
-    # ax.legend()
-    # ax.grid(True)
-    
-    # ax.set_ylim(min(valores_angulo_solido) - 0.1, max(valores_angulo_solido) + 0.1)
+def gerar_arquivo(sorteios, angulo_solido, tempo_sorteio, arquivo):
+    erro = angulo_solido - np.pi
+    with open(arquivo, 'a') as f:
+        f.write(f'{sorteios}        {angulo_solido}        {erro}          {tempo_sorteio}\n')
